a5818448b589a221f2ab82518ca6da0e3ad4058a
Nina NJul <nina.dev.web@gmail.com>
2020-12-26 18:28:43 +0200

    feat: add Inhabitant

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
+const Inhabitant = function (x, y, speed) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+};
+
+// Renders the enemy and the player into the game
+Inhabitant.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Inhabitant.prototype.update = function (dt) {};
+
 // Enemies our player must avoid
-var Enemy = function (x, y, speed) {
+const Enemy = function (x, y, speed) {
+  Inhabitant.call(this, x, y, speed);
   // The following variables are used to determine the x and y axis and speed of the enemy
   this.x = x;
   this.y = y;
@@ -8,6 +22,8 @@ var Enemy = function (x, y, speed) {
   // The image of the enemy of cockroach that is added to the playing field
   this.sprite = 'images/enemy-bug.png';
 };
+
+Enemy.prototype = Object.create(Inhabitant.prototype);
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -18,7 +34,7 @@ Enemy.prototype.update = function (dt) {
   // Once enemies are off the canvas, they reappear randomly with different speeds
   if (this.x > 510) {
     this.x = -50;
-    this.speed = 100 + Math.floor(Math.random() * 222);
+    this.speed = 100 + Math.floor(Math.random() * 400);
   }
 
   // Checks for collisions between the player and the enemies
@@ -33,27 +49,18 @@ Enemy.prototype.update = function (dt) {
   }
 };
 
-// Renders the enemy into the game
-Enemy.prototype.render = function () {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Player class focusing on x and y axis
-var Player = function (x, y) {
-  // Variables for the player to move along x and y axis
+// Player
+const Player = function (x, y) {
+  Inhabitant.call(this, x, y);
+  // The following variables are used to determine the x and y axis
   this.x = x;
   this.y = y;
 
-  //The image of the player of horn-girl is added to the playing field
-  this.player = 'images/char-horn-girl.png';
+  // The image of the player that is added to the playing field
+  this.sprite = 'images/char-horn-girl.png';
 };
 
-Player.prototype.update = function (dt) {};
-
-// Renders the image of the user into the game
-Player.prototype.render = function () {
-  ctx.drawImage(Resources.get(this.player), this.x, this.y);
-};
+Player.prototype = Object.create(Inhabitant.prototype);
 
 // Allows the user to use the arrow keys to jump from tile to tile
 Player.prototype.handleInput = function (keyPress) {
@@ -91,10 +98,10 @@ Player.prototype.handleInput = function (keyPress) {
 };
 
 // All enemies are placed in an array
-var allEnemies = [];
+const allEnemies = [];
 
 // Location of the 3 enemies on the y axis located on the stone road
-var enemyLocation = [63, 147, 230];
+const enemyLocation = [63, 147, 230];
 
 // For each enemy located on the y axis from 0 on the x axis move at a speed of 200
 // Until randomly regenerated in the enemy update function above
@@ -103,8 +110,8 @@ enemyLocation.forEach(function (locationY) {
   allEnemies.push(enemy);
 });
 
-// The starting location of the player is located at x=200, y=405
-var player = new Player(202, 405);
+// The starting location of the player is located at x = 202, y = 405
+const player = new Player(202, 405);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method.
